989fea6d4eb596f599d75556da552232ddbc7d51
Andy Guajardo <andymatt1225@gmail.com>
2025-03-21 13:04:28 -0700

    additional rec problems/tests

--- src/recursionPractice.js
+++ src/recursionPractice.js
@@ -1,22 +1,51 @@
 //recursion practice problems
 
-function palindromeRecursive(str) {
-    if (str.length <= 1) {
-        return true
-    }
-    if (str[0] !== str[str.length - 1]) {
-        return false
-    }
-    return palindromeRecursive(str.slice(1, str.length - 1))
+//given a number, count down from that number to 0
+// will only be given positive numbers
+// answer should be an array that counts down from the number to 0
+function countdown(num) {
+    
+}
+    
+
+//given a string, return true if the string is a palindrome
+//return false if the string is not a palindrome
+function palindromeChecker(str) {
+   
 }
 
-function reverseStringRecursive(str) {
-    if (str.length <= 1) {
-        return str
-    }
-    return reverseStringRecursive(str.slice(1)) + str[0]
+//given a string, return the reverse of the string
+//return an empty string if the string is empty
+function reverseString(str) {
+    
 }
 
-function factorialRecursive(num) {
+//given a number, return the factorial of the number
+function factorial(num) {
+    
+}
 
+//given a number, return the sum of its digits
+//Ex: 123 -> 6, 456 -> 15
+function sumDigits(num) {
+   
+}
+
+//given an array of numbers, return the sum of all the numbers
+//return 0 if the array is empty
+function arraySum(arr) {
+   
+}
+
+//given a number, return true if the number is prime
+//return false if the number is not prime
+function isPrime(num) {
+    
+        
+}
+
+//given a number, return the nth fibonacci number
+//
+function fibonacci(num) {
+    
 }
